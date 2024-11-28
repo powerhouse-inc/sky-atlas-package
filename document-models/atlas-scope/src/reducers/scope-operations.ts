648b1ef47f09df0f7e31e5a51ca3d4b76702655b
apeironmaker <123967815+apeiron-M@users.noreply.github.com>
2024-11-28 14:24:00 +0100

    revised atlas-scope

---
+++ document-models/atlas-scope/src/reducers/scope-operations.ts
@@ -0,0 +1,14 @@
+/**
+ * This is a scaffold file meant for customization:
+ * - modify it by implementing the reducer functions
+ * - delete the file and run the code generator again to have it reset
+ */
+
+import { AtlasScopeScopeOperationsOperations } from "../../gen/scope-operations/operations";
+
+export const reducer: AtlasScopeScopeOperationsOperations = {
+  updateScopeOperation(state, action, dispatch) {
+    // TODO: Implement "updateScopeOperation" reducer
+    throw new Error('Reducer "updateScopeOperation" not yet implemented');
+  },
+};
