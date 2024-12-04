2b3596132b066cf00931f0d309242c5a8f265750
apeironmaker <123967815+apeiron-M@users.noreply.github.com>
2024-12-04 11:52:17 +0100

    AtlasScope as a starting point, removed atlas-article, general clean up

--- document-models/atlas-scope/src/reducers/scope.ts
+++ document-models/atlas-scope/src/reducers/scope.ts
@@ -7,8 +7,50 @@
 import { AtlasScopeScopeOperations } from "../../gen/scope/operations";
 
 export const reducer: AtlasScopeScopeOperations = {
-  setNameOperation(state, action, dispatch) {
-    // TODO: Implement "setNameOperation" reducer
-    throw new Error('Reducer "setNameOperation" not yet implemented');
+  addScopeOperation(state, action, dispatch) {
+    state.scopes.push({
+      name: action.input.name,
+      docNo: action.input.docNo,
+      content: action.input.content,
+      masterStatus: action.input.masterStatus,
+      globalTags: action.input.globalTags,
+      originalContextData: action.input.originalContextData,
+      provenance: action.input.provenance,
+      notionId: action.input.notionId,
+    });
+  },
+  updateScopeOperation(state, action, dispatch) {
+    const scope = state.scopes.find(scope => scope.docNo === action.input.docNo);
+    if (!scope) {
+      throw new Error(`Scope with docNo ${action.input.docNo} not found`);
+    }
+    if (action.input.name) {
+      scope.name = action.input.name;
+    }
+    if (action.input.content) {
+      scope.content = action.input.content;
+    }
+    if (action.input.masterStatus) {
+      scope.masterStatus = action.input.masterStatus;
+    }
+    if (action.input.globalTags) {
+      scope.globalTags = action.input.globalTags;
+    }
+    if (action.input.originalContextData) {
+      scope.originalContextData = action.input.originalContextData;
+    }
+    if (action.input.provenance) {
+      scope.provenance = action.input.provenance;
+    }
+    if (action.input.notionId) {
+      scope.notionId = action.input.notionId;
+    }
+  },
+  deleteScopeOperation(state, action, dispatch) {
+    const scopeIndex = state.scopes.findIndex(scope => scope.docNo === action.input.docNo);
+    if (scopeIndex === -1) {
+      throw new Error(`Scope with docNo ${action.input.docNo} not found`);
+    }
+    state.scopes.splice(scopeIndex, 1);
   },
 };
--- document-models/atlas-scope/src/tests/scope.test.ts
+++ document-models/atlas-scope/src/tests/scope.test.ts
@@ -7,7 +7,7 @@ import { generateMock } from "@powerhousedao/codegen";
 import { utils as documentModelUtils } from "document-model/document";
 
 import utils from "../../gen/utils";
-import { z, SetNameInput } from "../../gen/schema";
+import { z, AddScopeInput, UpdateScopeInput, DeleteScopeInput } from "../../gen/schema";
 import { reducer } from "../../gen/reducer";
 import * as creators from "../../gen/scope/creators";
 import { AtlasScopeDocument } from "../../gen/types";
@@ -19,17 +19,67 @@ describe("Scope Operations", () => {
     document = utils.createDocument();
   });
 
-  it("should handle setName operation", () => {
-    // generate a random id
-    // const id = documentModelUtils.hashKey();
+  it("should handle addScope operation", () => {
+    const input: AddScopeInput = generateMock(z.AddScopeInputSchema());
 
-    const input: SetNameInput = generateMock(z.SetNameInputSchema());
-
-    const updatedDocument = reducer(document, creators.setName(input));
+    const updatedDocument = reducer(document, creators.addScope(input));
 
     expect(updatedDocument.operations.global).toHaveLength(1);
-    expect(updatedDocument.operations.global[0].type).toBe("SET_NAME");
+    expect(updatedDocument.operations.global[0].type).toBe("ADD_SCOPE");
     expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
     expect(updatedDocument.operations.global[0].index).toEqual(0);
+    expect(updatedDocument.state.global.scopes).toHaveLength(1);
+  });
+
+  it("should handle updateScope operation", () => {
+    const addInput: AddScopeInput = generateMock(z.AddScopeInputSchema());
+    const updateInput: UpdateScopeInput = generateMock(z.UpdateScopeInputSchema());
+
+    const createdDocument = reducer(document, creators.addScope(addInput));
+    const updatedDocument = reducer(createdDocument, creators.updateScope(updateInput));
+
+    expect(updatedDocument.operations.global).toHaveLength(2);
+    expect(updatedDocument.operations.global[1].type).toBe("UPDATE_SCOPE");
+    expect(updatedDocument.operations.global[1].input).toStrictEqual(updateInput);
+    expect(updatedDocument.operations.global[1].index).toEqual(1);
+
+    const updatedScope = updatedDocument.state.global.scopes.find(scope => scope.docNo === updateInput.docNo);
+    expect(updatedScope).toBeDefined();
+
+    if (updateInput.name) {
+      expect(updatedScope.name).toBe(updateInput.name);
+    }
+    if (updateInput.content) {
+      expect(updatedScope.content).toBe(updateInput.content);
+    }
+    if (updateInput.masterStatus) {
+      expect(updatedScope.masterStatus).toStrictEqual(updateInput.masterStatus);
+    }
+    if (updateInput.globalTags) {
+      expect(updatedScope.globalTags).toStrictEqual(updateInput.globalTags);
+    }
+    if (updateInput.originalContextData) {
+      expect(updatedScope.originalContextData).toStrictEqual(updateInput.originalContextData);
+    }
+    if (updateInput.provenance) {
+      expect(updatedScope.provenance).toBe(updateInput.provenance);
+    }
+    if (updateInput.notionId) {
+      expect(updatedScope.notionId).toBe(updateInput.notionId);
+    }
+  });
+
+  it("should handle deleteScope operation", () => {
+    const addInput: AddScopeInput = generateMock(z.AddScopeInputSchema());
+    const deleteInput: DeleteScopeInput = { docNo: addInput.docNo };
+
+    const createdDocument = reducer(document, creators.addScope(addInput));
+    const updatedDocument = reducer(createdDocument, creators.deleteScope(deleteInput));
+
+    expect(updatedDocument.operations.global).toHaveLength(2);
+    expect(updatedDocument.operations.global[1].type).toBe("DELETE_SCOPE");
+    expect(updatedDocument.operations.global[1].input).toStrictEqual(deleteInput);
+    expect(updatedDocument.operations.global[1].index).toEqual(1);
+    expect(updatedDocument.state.global.scopes).toHaveLength(0);
   });
 });
