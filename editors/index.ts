2c3ccf14bcee9043ad985d9469300abcc21b802b
liberuum <liberuum@powerhouse.inc>
2024-12-13 14:03:04 +0700

    update scope op + fix link

--- editors/index.ts
+++ editors/index.ts
@@ -1,5 +1,5 @@
-export { module as AtlasScope } from "./atlas-scope";
-export { module as AtlasArticle } from "./atlas-article";
+export { module as AtlasScope } from "./scope";
+export { module as AtlasArticle } from "./article";
 export { module as Section } from "./section";
 export { module as Core } from "./core";
 export { module as Controller } from "./controller";
