4570f3d744aa1e00f7aa3bcbc4bd7b5e412b1616
liberuum <liberom@proton.me>
2025-01-08 11:25:13 +0000

    use only 2 doc models due to lack of gql stitching

--- document-models/index.ts
+++ document-models/index.ts
@@ -1,5 +1,2 @@
 export { module as AtlasScope } from "./atlas-scope";
-export { module as AtlasExploratory } from "./atlas-exploratory";
 export { module as AtlasFoundation } from "./atlas-foundation";
-export { module as AtlasGround } from "./atlas-ground";
-export { module as AtlasMultiParent } from "./atlas-multi-parent";
